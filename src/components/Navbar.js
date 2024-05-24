e36dc58a6a529d52645e53a0b60630957a78f4dc
Saiganesh Pondhuru <pondhurusaiganesh@gmail.com>
2024-05-24 11:39:30 +0530

    24-05-24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,9 +63,9 @@ const Navbar = () => {
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
 
       <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
-        <span className={`bg-dak dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
-        <span className={`bg-dak dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
-        <span className={`bg-dak dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+        <span className={`bg-dark dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+        <span className={`bg-dark dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+        <span className={`bg-dark dark:bg-light block transition-all duaration-3000  ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
       </button>
 
       <div className="w-full flex justify-between items-center lg:hidden">
